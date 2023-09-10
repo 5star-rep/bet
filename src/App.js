c9271fe6af929b297ea895d6dd0938de3f8bb2bc
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-09-10 07:48:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.png", "_blank");
+                  window.open("/config/roadmap.jpg", "_blank");
                 }}
                 style={{
                   margin: "5px",
